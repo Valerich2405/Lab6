7ea05a5530837bd7b697c6081575c449937386de
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2022-11-06 13:45:51 +0200

    Add MyClass

---
+++ MyClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyClass
+{
+    class MyClass<T>
+    {
+        public static T FactoryMethod<T>() where T : new()
+        {
+            return new T();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int>.FactoryMethod<int>();
+
+            Console.ReadKey();
+        }
+    }
+}
